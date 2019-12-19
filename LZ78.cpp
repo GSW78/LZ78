96051b89425890c897b2e8ea0a4a39326d804b48
Tzipporah <tzbat98@gmail.com>
2019-12-19 12:11:13 +0200

    Add() function is finished.

--- LZ78.cpp
+++ LZ78.cpp
@@ -9,6 +9,23 @@ LZ78::LZ78(std::string input,  int lastCharsIndex)
     tree = NULL;
 }
 
+/*
+@param node{int}
+@param edge{char} 
+Description : The function adds a new node to the tree.
+*/
+void LZ78::add(int node, char edge)
+{
+    // Create a new node for the tree.
+    Pair *newNode = new Pair(node,edge);
+    
+    // The first node is added to the tree: Create a new array.
+    if (tree == NULL)
+        tree = (Pair *)malloc(sizeof(Pair));
+    // Any other case: make the array of the pairs bigger.
+    else tree = (Pair *)realloc(tree, sizeof(Pair));
+
+}
 LZ78::~LZ78()
 {
 }
