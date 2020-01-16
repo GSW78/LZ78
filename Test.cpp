bce7f1107f428419e0f4d35502914226cc829ea8
Tzipporah <tzbat98@gmail.com>
2020-01-16 16:18:07 +0200

    Dassi added Test class

--- Test.cpp
+++ Test.cpp
@@ -1,2 +1,33 @@
 #include "Test.h"
+#include <iostream>
+#include <cctype>
 
+bool Test::checkSize(std::string input){
+    if(input.length() > MAX_LENGTH)
+        return false;
+    return true;
+}
+
+bool Test::checkChars(std::string input){
+
+    for(int i = 0; i < input.length(); i++){
+        char char_to_check = std::tolower(input [i]);
+
+        if(char_to_check < 'a' || char_to_check > 'z'){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+bool Test::checkAll(std::string input){
+    bool flag = true;
+    std::cout<<"flag "<<flag<<"\n";
+    if(!checkSize(input))
+        flag = false;
+    if(!checkChars(input))
+        flag = false;
+    return flag;
+}
--- Test.h
+++ Test.h
@@ -1,15 +1,14 @@
 #ifndef Test
 #include <string>
-#define MAX_LENGTH 2000
+#define MAX_LENGTH 360
+
 class Test
 {
-private:
-    
-    bool checkChars();
-    bool checkAll();
-public:
-    Test(std::string input);
-    bool checkSize();
-      
+    private:
+        bool checkChars(std::string input);
+        bool checkSize(std::string input);
+    public:
+        //Test(std::string input);
+        bool checkAll(std::string input);
 };
 #endif
