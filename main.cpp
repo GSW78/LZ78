77e7cf05e81586b17cd67a29918e46b537ab7fb6
saraboro <54246527+saraboro@users.noreply.github.com>
2020-01-21 16:18:53 +0200

    Update main.cpp
    Beautify code.

--- main.cpp
+++ main.cpp
@@ -10,27 +10,27 @@ int main(int argc, char *argv[])
 {
 	//Get input from file
 	std::ifstream inFile;
-    inFile.open(argv[1]);
+	inFile.open(argv[1]);
 	if (!inFile) {
-        std::cout << "Unable to open file.";
-        exit(1);
-    }
-    std::stringstream strStream;
-    strStream << inFile.rdbuf();
-    std::string str = strStream.str();
+		std::cout << "Unable to open file";
+		exit(1);
+	}
+	std::stringstream strStream;
+	strStream << inFile.rdbuf();
+	std::string str = strStream.str();
 	inFile.close();
 	
 	//validate input
 	Test test;
 	if(!test.checkAll(str)){
-		std::cout << "Input: "<< str <<" is in incorrect format.";
-		exit(1);
+		std::cout << "input: "<< str <<" is in incorrect format.";
+	exit(1);
 	}
-	
-	//create tree, encode, print
-    LZ78 *tree = new LZ78("a", 2);
-    tree->encode(str);
-    tree->printTree();
 
-    return 1;
+	//create tree, encode, print
+	LZ78 *tree = new LZ78("a", 2);
+	tree->encode(str);
+	tree->printTree();
+	
+	return 1;
 }
