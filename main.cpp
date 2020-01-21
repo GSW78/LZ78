dbdc8ec3aa0151e5f9a4d20bfd5b59ade17205be
saraboro <54246527+saraboro@users.noreply.github.com>
2020-01-21 16:01:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
-#include "LZ78.h"
 #include <string>
+#include <fstream>
+#include <streambuf>
+#include <sstream>
+#include "LZ78.h"
+#include "Test.h"
 
-
-int main()
+int main(int argc, char *argv[])
 {
+	//Get input from file
+	std::ifstream inFile;
+    inFile.open(argv[1]);
+	if (!inFile) {
+        std::cout << "Unable to open file.";
+        exit(1);
+    }
+    std::stringstream strStream;
+    strStream << inFile.rdbuf();
+    std::string str = strStream.str();
+	inFile.close();
+	
+	//validate input
+	Test test;
+	if(!test.checkAll(str)){
+		std::cout << "Input: "<< str <<" is in incorrect format.";
+		exit(1);
+	}
+	
+	//create tree, encode, print
     LZ78 *tree = new LZ78("a", 2);
-    // std::cout<< "HI"<<std::endl;
-    // std::cout<< tree->getSizeOfTree() <<std::endl;
-
-    // tree->add(0,'a', 1);
-    // tree->add(1,'c', 2);
-    // tree->add(0,'g', 3);
-    // tree->add(2,'g', 4);
-    // tree->add(1,'t', 5);
-    // tree->add(5,'b', 6);
-    // tree->add(6,'g', 7);
-    // tree->add(3,'a', 8);
-    // tree->add(5,'a', 9);
-
-    tree->encode("aacgacgat");
+    tree->encode(str);
     tree->printTree();
-
-    // tree->maxCode("atb");
 
     return 1;
 }
